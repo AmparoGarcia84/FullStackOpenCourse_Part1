2528f84107b4660de08ffef0e4160ec1e0bd7410
amgarvi <amparo@handpoint.com>
2026-01-30 13:28:06 +0100

    Exercise 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,61 +1,47 @@
-const Header = (props) => {
-  console.log('Header props:', props)
+import { useState } from 'react'
+
+const Header = ({ text }) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{text}</h1>
   )
 }
 
-const Content = (props) => {
-  console.log('Content props', props)
+const Button = ({ handleClick, text }) => {
   return (
-    <div>
-      <Part part={props.parts[0]}/>
-      <Part part={props.parts[1]}/>
-      <Part part={props.parts[2]}/>
-    </div>
-  )
-}
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)}
 
-const Total = (props) => {
-  console.log('Total props:', props)
-  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+const StatisticLine = ({ text, value }) => {
   return (
-    <p>Number of exercises {total}</p>
-  )
-}
-
-const Part = (props) => {
-  console.log('Part props:', props)
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Header text='Give feedback' />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Header text='Statistics' />
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+        </tbody>
+      </table>
     </div>
   )
 }
