b8ae5ce52712a4d2db94c8931630aec215432d18
amgarvi <amparo@handpoint.com>
2026-02-02 12:43:24 +0100

    Exercise 1.13

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -26,6 +26,7 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   const [selected, setSelected] = useState(0)
 
@@ -35,9 +36,18 @@ const App = () => {
     setSelected(randomIndex)
   }
 
+  const handleClickVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+    console.log(newVotes)
+  }
+
   return (
     <div>
       <Anecdote anecdote={anecdotes[selected]} />
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick={handleClickVote} text='Vote' />
       <Button handleClick={handleClickAnecdote} text='Next anecdote' />
     </div>
   )
